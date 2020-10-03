0a7429bd4251ffac548872eed44b10a1c4b80694
tongji4m3 <1254931237@qq.com>
2020-10-03 11:08:45 +0800

    Create Singleton.java

---
+++ src/SingletonPattern/Singleton.java
@@ -0,0 +1,20 @@
+package SingletonPattern;
+
+public class Singleton
+{
+    private static Singleton uniqueInstance;
+
+    private Singleton()
+    {
+
+    }
+
+    public static Singleton getInstance()
+    {
+        if(uniqueInstance==null)
+        {
+            uniqueInstance = new Singleton();
+        }
+        return uniqueInstance;
+    }
+}
