0cd889d93f9009ac6f47c418839774e7be5ef2a8
tongji4m3 <1254931237@qq.com>
2020-10-03 11:21:21 +0800

    Update Singleton.java

--- src/SingletonPattern/Singleton.java
+++ src/SingletonPattern/Singleton.java
@@ -2,7 +2,8 @@ package SingletonPattern;
 
 public class Singleton
 {
-    private static Singleton uniqueInstance;
+    //保证线程的可见性
+    private volatile static Singleton uniqueInstance;
 
     private Singleton()
     {
@@ -13,7 +14,17 @@ public class Singleton
     {
         if(uniqueInstance==null)
         {
-            uniqueInstance = new Singleton();
+            //估计只有几个线程会进到这里
+            synchronized (Singleton.class)
+            {
+                //这些线程逐个进来,但是只有第一个线程会进入到下面的if
+                //其他的线程进来时,已经初始化完毕了,就不好初始化多个实例
+                if(uniqueInstance==null)
+                {
+                    //第一个线程进来,初始化
+                    uniqueInstance = new Singleton();
+                }
+            }
         }
         return uniqueInstance;
     }
